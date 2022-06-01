6dff705526c9a8562f642023642755e724fcfbb9
Shadab <salam@alazka.ai>
2022-06-01 19:02:26 +0530

    added truncateString method

--- console/src/pages/workspace/DatabaseBrowser/QueryPane.tsx
+++ console/src/pages/workspace/DatabaseBrowser/QueryPane.tsx
@@ -11,6 +11,7 @@ import { QueryExecutionResult } from "../../../models/DatabaseBrowser";
 import Connections from "../../../services/Connections";
 import { InternalServerError } from "../../../services/SparkService";
 import { Resizable } from "re-resizable";
+import { truncateString } from "../../../components/utils/utils";
 
 type QueryResult = { err?: string; result?: QueryExecutionResult };
 
@@ -45,7 +46,7 @@ const QueryPane: FC<{
     Connections.getQuery(connectionId, queryId, (q) => {
       setQueryView({ ...queryView, loading: false, savedQuery: q.text, currentState: q.text });
     });
-  }, [queryId]);
+  }, [queryId, queryView.savedQuery]);
 
   const closeSaveAsModal = () => {
     modalForm.resetFields();
@@ -230,9 +231,26 @@ const QueryPane: FC<{
                           pagination={false}
                           className='tbl-data'
                           style={{ minHeight: "20vh", backgroundColor: "#fff" }}>
-                          {qr.result.columns.map((c, i) => (
-                            <Column className='table-cell-light' key={i.toString()} title={c.name.toUpperCase()} dataIndex={c.name} />
-                          ))}
+                          {qr.result.columns.map((c, i) => {
+                            if (c.name.toUpperCase() === "PROPERTIES") {
+                              return (
+                                <Column
+                                  className='table-cell-light'
+                                  key={i.toString()}
+                                  title={c.name.toUpperCase()}
+                                  render={(v) => (
+                                    <Tooltip overlayInnerStyle={{ width: "600px" }} title={v.properties}>
+                                      {truncateString(v.properties, 50)}
+                                    </Tooltip>
+                                  )}
+                                />
+                              );
+                            } else {
+                              return (
+                                <Column className='table-cell-light' key={i.toString()} title={c.name.toUpperCase()} dataIndex={c.name} />
+                              );
+                            }
+                          })}
                         </Table>
                       )}
                     </TabPane>
