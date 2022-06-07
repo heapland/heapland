2c45af720878d39b76c1f92ae299bdecc80cb2e6
Shadab <salam@alazka.ai>
2022-06-07 17:09:24 +0530

    added row selection method and add new row

--- console/src/pages/workspace/DatabaseBrowser/TablePane.tsx
+++ console/src/pages/workspace/DatabaseBrowser/TablePane.tsx
@@ -1,6 +1,6 @@
-import { Select, Skeleton, Table, Tabs } from "antd";
+import { Select, Skeleton, Table, Tabs, Form, Input, InputNumber, Popconfirm } from "antd";
 import Column from "antd/lib/table/Column";
-import React, { FC, ReactNode, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { QueryExecutionResult } from "../../../models/DatabaseBrowser";
 import Connections from "../../../services/Connections";
 import TableActionHeader from "./TableActionHeader";
@@ -31,10 +31,24 @@ const TablePane: React.FC<{ schema: string; name: string; connectionId: number; 
     setDownloadModal(false);
   };
 
+  const rowSelection = {
+    onChange: (selectedRowKeys: React.Key[], selectedRows: any[]) => {
+      console.log(`selectedRowKeys: ${selectedRowKeys}`, "selectedRows: ", selectedRows);
+    },
+  };
+
+  const handleAdd = () => {
+    let newData: any = {};
+    tableData.result.columns.map((c) => {
+      newData[c.name] = "Add Data";
+    });
+    setTableData({ ...tableData, loading: false, result: { ...tableData.result, result: [...tableData.result.result, newData] } });
+  };
+
   return (
     <>
       <TableActionHeader
-        onAddRow={() => {}}
+        onAddRow={handleAdd}
         onDeleteRow={() => {}}
         onRefres={() => setRefres(!refres)}
         onDownloadDataModal={onDownloadDataModal}
@@ -42,6 +56,10 @@ const TablePane: React.FC<{ schema: string; name: string; connectionId: number; 
       />
       <Skeleton loading={tableData.loading} active paragraph={{ rows: 4 }}>
         <Table
+          rowSelection={{
+            type: "checkbox",
+            ...rowSelection,
+          }}
           dataSource={tableData.result ? tableData.result.result : []}
           rowKey={(c: any) => c.id}
           scroll={{ x: "calc(100vw - 470px)" }}
