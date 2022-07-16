1dc90fed187ec98010620cce69e5fe9ad1a572c5
Shadab Alam <shadab19it@gmail.com>
2022-07-16 13:33:07 +0530

    fix scroll height of the database objects browser

--- console/src/components/utils/utils.ts
+++ console/src/components/utils/utils.ts
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { MessageApi } from "antd/lib/message";
 import { QueryExecutionResult } from "../../models/DatabaseBrowser";
 import { EditorLang } from "../../pages/workspace/DatabaseBrowser/DatabaseBrowser";
@@ -357,4 +358,32 @@ export const getLangDefinition = (editorLang: EditorLang) => {
       ],
     },
   };
+};
+
+export const useWindowDimensions = () => {
+  const hasWindow = typeof window !== "undefined";
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : null;
+    const height = hasWindow ? window.innerHeight : null;
+    return {
+      width,
+      height,
+    };
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    if (hasWindow) {
+      const handleResize = () => {
+        setWindowDimensions(getWindowDimensions());
+      };
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, [hasWindow]);
+
+  return windowDimensions;
 };
